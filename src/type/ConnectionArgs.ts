c0a527d9f76d9abcaeec52d326e754ae18d73964
Simon Garner <simon.garner@madscience.co.nz>
2021-08-25 10:47:05 +1200

    Add class-validator decorators to ConnectionArgs

--- src/type/ConnectionArgs.ts
+++ src/type/ConnectionArgs.ts
@@ -1,31 +1,47 @@
 import * as GQL from '@nestjs/graphql';
+import * as Validate from 'class-validator';
 
 @GQL.ArgsType()
 export class ConnectionArgs {
+  @Validate.IsInt()
+  @Validate.Min(1)
+  @Validate.IsOptional()
   @GQL.Field(_type => GQL.Int, {
     nullable: true,
     description: 'Retrieve page of edges by fixed offset page number.',
   })
   public page?: number;
 
+  @Validate.IsString()
+  @Validate.MinLength(1)
+  @Validate.IsOptional()
   @GQL.Field(_type => String, {
     nullable: true,
     description: 'Retrieve page of edges before opaque cursor.',
   })
   public before?: string;
 
+  @Validate.IsString()
+  @Validate.MinLength(1)
+  @Validate.IsOptional()
   @GQL.Field(_type => String, {
     nullable: true,
     description: 'Retrieve page of edges after opaque cursor.',
   })
   public after?: string;
 
+  @Validate.IsInt()
+  @Validate.Min(1)
+  @Validate.IsOptional()
   @GQL.Field(_type => GQL.Int, {
     nullable: true,
     description: 'Retrieve first `n` edges.',
   })
   public first?: number;
 
+  @Validate.IsInt()
+  @Validate.Min(1)
+  @Validate.IsOptional()
   @GQL.Field(_type => GQL.Int, {
     nullable: true,
     description: 'Retrieve last `n` edges.',
