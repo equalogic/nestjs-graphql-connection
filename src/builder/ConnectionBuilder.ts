3438b9a67749f35ae894e704de8a08938ae5efa1
Simon Garner <simon@equalogic.com>
2022-06-11 13:23:06 +1200

    Add OffsetPaginatedConnectionBuilder

--- src/builder/ConnectionBuilder.ts
+++ src/builder/ConnectionBuilder.ts
@@ -14,15 +14,84 @@ export abstract class ConnectionBuilder<
   TNode,
   TCursor extends Cursor = Cursor,
 > {
-  public edgesPerPage: number;
+  public edgesPerPage: number = 20;
   public beforeCursor?: TCursor;
   public afterCursor?: TCursor;
 
-  public constructor(
+  public constructor(connectionArgs: ConnectionArgs, options: ConnectionBuilderOptions = {}) {
+    this.applyConnectionArgs(connectionArgs, options);
+  }
+
+  public abstract createConnection(fields: { edges: TEdge[]; pageInfo: PageInfo }): TConnection;
+
+  public abstract createEdge(fields: { node: TNode; cursor: string }): TEdge;
+
+  public abstract createCursor(node: TNode, index: number): TCursor;
+
+  public decodeCursor(encodedString: string): TCursor {
+    return Cursor.fromString(encodedString) as TCursor;
+  }
+
+  public createPageInfo({
+    edges,
+    hasNextPage,
+    hasPreviousPage,
+    totalEdges,
+  }: {
+    edges: TEdge[];
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+    totalEdges?: number;
+  }): PageInfo {
+    return {
+      startCursor: edges.length > 0 ? edges[0].cursor : null,
+      endCursor: edges.length > 0 ? edges[edges.length - 1].cursor : null,
+      hasNextPage,
+      hasPreviousPage,
+      totalEdges,
+    };
+  }
+
+  public build({
+    nodes,
+    totalEdges,
+    hasNextPage,
+    hasPreviousPage,
+    createConnection = this.createConnection.bind(this),
+    createEdge = this.createEdge.bind(this),
+    createCursor = this.createCursor.bind(this),
+  }: {
+    nodes: TNode[];
+    totalEdges?: number;
+    hasNextPage?: boolean;
+    hasPreviousPage?: boolean;
+    createConnection?: (fields: { edges: TEdge[]; pageInfo: PageInfo }) => TConnection;
+    createEdge?: (fields: { node: TNode; cursor: string }) => TEdge;
+    createCursor?: (node: TNode, index: number) => TCursor;
+  }): TConnection {
+    const edges = nodes.map((node, index) =>
+      createEdge({
+        node,
+        cursor: createCursor(node, index).encode(),
+      }),
+    );
+
+    return createConnection({
+      edges,
+      pageInfo: this.createPageInfo({
+        edges,
+        totalEdges,
+        hasNextPage: hasNextPage ?? (totalEdges != null && totalEdges > edges.length),
+        hasPreviousPage: hasPreviousPage ?? (this.afterCursor != null || this.beforeCursor != null),
+      }),
+    });
+  }
+
+  protected applyConnectionArgs(
     { page, first, last, before, after }: ConnectionArgs,
-    { defaultEdgesPerPage = 20, maxEdgesPerPage = 100, allowReverseOrder = true }: ConnectionBuilderOptions = {},
-  ) {
-    this.edgesPerPage = defaultEdgesPerPage;
+    { defaultEdgesPerPage, maxEdgesPerPage = 100, allowReverseOrder = true }: ConnectionBuilderOptions = {},
+  ): void {
+    this.edgesPerPage = defaultEdgesPerPage ?? this.edgesPerPage;
 
     if (page != null) {
       throw new ConnectionArgsValidationError('This connection does not support the "page" argument for pagination.');
@@ -68,70 +137,5 @@ export abstract class ConnectionBuilder<
 
     this.beforeCursor = before != null ? this.decodeCursor(before) : undefined;
     this.afterCursor = after != null ? this.decodeCursor(after) : undefined;
-  }
-
-  public abstract createConnection(fields: { edges: TEdge[]; pageInfo: PageInfo }): TConnection;
-
-  public abstract createEdge(fields: { node: TNode; cursor: string }): TEdge;
-
-  public abstract createCursor(node: TNode, offset: number): TCursor;
-
-  public decodeCursor(encodedString: string): TCursor {
-    return Cursor.fromString(encodedString) as TCursor;
-  }
-
-  public createPageInfo({
-    edges,
-    hasNextPage,
-    hasPreviousPage,
-    totalEdges,
-  }: {
-    edges: TEdge[];
-    hasNextPage: boolean;
-    hasPreviousPage: boolean;
-    totalEdges?: number;
-  }): PageInfo {
-    return {
-      startCursor: edges.length > 0 ? edges[0].cursor : null,
-      endCursor: edges.length > 0 ? edges[edges.length - 1].cursor : null,
-      hasNextPage,
-      hasPreviousPage,
-      totalEdges,
-    };
-  }
-
-  public build({
-    nodes,
-    totalEdges,
-    hasNextPage,
-    hasPreviousPage,
-    createConnection = this.createConnection,
-    createEdge = this.createEdge,
-    createCursor = this.createCursor,
-  }: {
-    nodes: TNode[];
-    totalEdges?: number;
-    hasNextPage?: boolean;
-    hasPreviousPage?: boolean;
-    createConnection?: (fields: { edges: TEdge[]; pageInfo: PageInfo }) => TConnection;
-    createEdge?: (fields: { node: TNode; cursor: string }) => TEdge;
-    createCursor?: (node: TNode, offset: number) => TCursor;
-  }): TConnection {
-    const edges = nodes.map((node, index) =>
-      createEdge({
-        node,
-        cursor: createCursor(node, index).encode(),
-      }),
-    );
-
-    return createConnection({
-      edges,
-      pageInfo: this.createPageInfo({
-        edges,
-        totalEdges,
-        hasNextPage: hasNextPage ?? (totalEdges != null && totalEdges > edges.length),
-        hasPreviousPage: hasPreviousPage ?? (this.afterCursor != null || this.beforeCursor != null),
-      }),
-    });
   }
 }
---
+++ src/builder/OffsetPaginatedConnectionBuilder.spec.ts
@@ -0,0 +1,148 @@
+import { Cursor } from '../cursor/Cursor';
+import { createConnectionType, createEdgeType, PageInfo } from '../type';
+import { OffsetPaginatedConnectionBuilder } from './OffsetPaginatedConnectionBuilder';
+
+class TestNode {
+  id: string;
+}
+
+class TestEdge extends createEdgeType(TestNode) {}
+
+class TestConnection extends createConnectionType(TestEdge) {}
+
+class TestConnectionBuilder extends OffsetPaginatedConnectionBuilder<TestConnection, TestEdge, TestNode> {
+  public createConnection(fields: { edges: TestEdge[]; pageInfo: PageInfo }): TestConnection {
+    return new TestConnection(fields);
+  }
+
+  public createEdge(fields: { node: TestNode; cursor: string }): TestEdge {
+    return new TestEdge(fields);
+  }
+}
+
+describe('OffsetPaginatedConnectionBuilder', () => {
+  test('First page is built correctly', () => {
+    const builder = new TestConnectionBuilder({
+      first: 5,
+    });
+
+    expect(builder.edgesPerPage).toBe(5);
+    expect(builder.startOffset).toBe(0);
+    expect(builder.afterCursor).toBeUndefined();
+    expect(builder.beforeCursor).toBeUndefined();
+
+    const connection = builder.build({
+      totalEdges: 12,
+      nodes: [{ id: 'node1' }, { id: 'node2' }, { id: 'node3' }, { id: 'node4' }, { id: 'node5' }],
+    });
+
+    expect(connection).toMatchObject({
+      pageInfo: {
+        totalEdges: 12,
+        hasNextPage: true,
+        hasPreviousPage: false,
+        startCursor: new Cursor({ offset: 0 }).encode(),
+        endCursor: new Cursor({ offset: 4 }).encode(),
+      },
+      edges: [
+        { node: { id: 'node1' }, cursor: new Cursor({ offset: 0 }).encode() },
+        { node: { id: 'node2' }, cursor: new Cursor({ offset: 1 }).encode() },
+        { node: { id: 'node3' }, cursor: new Cursor({ offset: 2 }).encode() },
+        { node: { id: 'node4' }, cursor: new Cursor({ offset: 3 }).encode() },
+        { node: { id: 'node5' }, cursor: new Cursor({ offset: 4 }).encode() },
+      ],
+    });
+  });
+
+  test('Second page is built correctly', () => {
+    const builder = new TestConnectionBuilder({
+      first: 5,
+      after: new Cursor({ offset: 4 }).encode(),
+    });
+
+    expect(builder.edgesPerPage).toBe(5);
+    expect(builder.startOffset).toBe(5);
+    expect(builder.afterCursor).toMatchObject(new Cursor({ offset: 4 }));
+    expect(builder.beforeCursor).toBeUndefined();
+
+    const connection = builder.build({
+      totalEdges: 12,
+      nodes: [{ id: 'node6' }, { id: 'node7' }, { id: 'node8' }, { id: 'node9' }, { id: 'node10' }],
+    });
+
+    expect(connection).toMatchObject({
+      pageInfo: {
+        totalEdges: 12,
+        hasNextPage: true,
+        hasPreviousPage: true,
+        startCursor: new Cursor({ offset: 5 }).encode(),
+        endCursor: new Cursor({ offset: 9 }).encode(),
+      },
+      edges: [
+        { node: { id: 'node6' }, cursor: new Cursor({ offset: 5 }).encode() },
+        { node: { id: 'node7' }, cursor: new Cursor({ offset: 6 }).encode() },
+        { node: { id: 'node8' }, cursor: new Cursor({ offset: 7 }).encode() },
+        { node: { id: 'node9' }, cursor: new Cursor({ offset: 8 }).encode() },
+        { node: { id: 'node10' }, cursor: new Cursor({ offset: 9 }).encode() },
+      ],
+    });
+  });
+
+  test('Last page is built correctly', () => {
+    const builder = new TestConnectionBuilder({
+      first: 5,
+      after: new Cursor({ offset: 9 }).encode(),
+    });
+
+    expect(builder.edgesPerPage).toBe(5);
+    expect(builder.startOffset).toBe(10);
+    expect(builder.afterCursor).toMatchObject(new Cursor({ offset: 9 }));
+    expect(builder.beforeCursor).toBeUndefined();
+
+    const connection = builder.build({
+      totalEdges: 12,
+      nodes: [{ id: 'node11' }, { id: 'node12' }],
+    });
+
+    expect(connection).toMatchObject({
+      pageInfo: {
+        totalEdges: 12,
+        hasNextPage: false,
+        hasPreviousPage: true,
+        startCursor: new Cursor({ offset: 10 }).encode(),
+        endCursor: new Cursor({ offset: 11 }).encode(),
+      },
+      edges: [
+        { node: { id: 'node11' }, cursor: new Cursor({ offset: 10 }).encode() },
+        { node: { id: 'node12' }, cursor: new Cursor({ offset: 11 }).encode() },
+      ],
+    });
+  });
+
+  test('Empty result is built correctly', () => {
+    const builder = new TestConnectionBuilder({
+      first: 5,
+    });
+
+    expect(builder.edgesPerPage).toBe(5);
+    expect(builder.startOffset).toBe(0);
+    expect(builder.afterCursor).toBeUndefined();
+    expect(builder.beforeCursor).toBeUndefined();
+
+    const connection = builder.build({
+      totalEdges: 0,
+      nodes: [],
+    });
+
+    expect(connection).toMatchObject({
+      pageInfo: {
+        totalEdges: 0,
+        hasNextPage: false,
+        hasPreviousPage: false,
+        startCursor: null,
+        endCursor: null,
+      },
+      edges: [],
+    });
+  });
+});
